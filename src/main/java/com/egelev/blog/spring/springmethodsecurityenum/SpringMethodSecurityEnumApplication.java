882fa62b1bcd97d26f62b48cd585aa23788eaa78
Emil Gelev <emil.gelev@gmail.com>
2022-03-29 09:53:22 +0300

    Initial commit from Spring Initializer

---
+++ src/main/java/com/egelev/blog/spring/springmethodsecurityenum/SpringMethodSecurityEnumApplication.java
@@ -0,0 +1,13 @@
+package com.egelev.blog.spring.springmethodsecurityenum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMethodSecurityEnumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMethodSecurityEnumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egelev/blog/spring/springmethodsecurityenum/SpringMethodSecurityEnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egelev.blog.spring.springmethodsecurityenum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMethodSecurityEnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
